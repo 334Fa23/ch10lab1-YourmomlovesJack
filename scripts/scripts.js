a72514924b05c7e36a788c8124a302c2b5e3df2e
YourmomlovesJack <S539614@nwmissouri.edu>
2023-09-19 23:13:05 -0500

    labbed it up

---
+++ scripts/scripts.js
@@ -0,0 +1,18 @@
+/*
+Author: Jack Stafford
+Date:9-19-23
+File Name: script.js
+*/
+
+//Hamburger menu function
+Function hamburger () {
+
+var menu = document.getElementbyId("menu-links");
+}
+
+if (menu.style.display === "block") {
+  menu.style.display = "none";
+}
+else {
+menu.style.display = "block";
+}
